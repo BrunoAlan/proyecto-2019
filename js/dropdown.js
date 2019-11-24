4264057d066568929b8f6942456184566a2f706e
BrunoAlan <alan.brunoab@gmail.com>
2019-11-24 20:34:44 -0300

    Modulo estudiantes - Diseño Lu

---
+++ js/dropdown.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  
+     $('.drop li:has(ul)').click(function(e){
+         
+         if ($(this).hasClass('activado')){
+             $(this).removeClass('activado');
+             $(this).children('ul').slideUp();
+         }
+         else{
+             $('.drop li ul').slideUp();
+             $('.drop li').removeClass('activado');
+             $(this).addClass('activado');
+             $(this).children('ul').slideDown();
+            
+         }
+     }); 
+
+     
+
+});
+
